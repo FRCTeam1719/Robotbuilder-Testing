313aba45aa1543e5ffd1b5b1c8096ccfd43da1d1
jnmd1999 <jnmd1999@gmail.com>
2015-01-10 12:16:05 -0500

    Fixing the Drive choosing button, smartdashboard

--- TEST/src/org/usfirst/frc1719/TEST/subsystems/Pneumatics.java
+++ TEST/src/org/usfirst/frc1719/TEST/subsystems/Pneumatics.java
@@ -12,7 +12,7 @@
 package org.usfirst.frc1719.TEST.subsystems;
 
 import org.usfirst.frc1719.TEST.RobotMap;
-import org.usfirst.frc1719.TEST.commands.*;
+//import org.usfirst.frc1719.TEST.commands.*;
 import edu.wpi.first.wpilibj.*;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
