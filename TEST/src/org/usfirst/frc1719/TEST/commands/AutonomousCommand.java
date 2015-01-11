cfcde0e500b31309c3e4d7a9afd61a6604a2293a
aaronjeline <aaroneline.github@gmail.com>
2015-01-11 14:59:22 -0500

    Encoder accuracy measuring

--- TEST/src/org/usfirst/frc1719/TEST/commands/AutonomousCommand.java
+++ TEST/src/org/usfirst/frc1719/TEST/commands/AutonomousCommand.java
@@ -35,11 +35,14 @@ public class  AutonomousCommand extends Command {
 
     // Called just before this Command runs the first time
     // please can we just put this in the constructor
+    // NO
     protected void initialize() {
+    	
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	
     	Robot.drive.moveArcade(DEFAULT_SPEED, DEFAULT_ROTATION);
     }
 
